6a6a2832468099e98d818228b08d560759fb2992
iamkokymam@gmail.com <iamkokymam@gmail.com>
2024-07-14 09:37:37 +0300

    deleting unnecessary comments

--- main.js
+++ main.js
@@ -4,12 +4,7 @@ const findMean = (...nums)=> {
     return sum/nums.length;
 }
 const findSmallest = (arr) =>{
-    
-  
-    // Initialize the smallest value to the first element in the array
     let smallest = arr[0];
-  
-    // Iterate through the array and update the smallest value if a smaller value is found
     for (let i = 1; i < arr.length; i++) {
       if (arr[i] < smallest) {
         smallest = arr[i];
