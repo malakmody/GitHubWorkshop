1a659d3d16303326a375a37dbaffb3ec15563f0c
iamkokymam@gmail.com <iamkokymam@gmail.com>
2024-07-14 09:32:48 +0300

    find the samllest in an array

--- main.js
+++ main.js
@@ -3,5 +3,18 @@ const findMean = (...nums)=> {
     const sum = nums.reduce((prev,curr)=>prev+curr)
     return sum/nums.length;
 }
-console.log(
-    findMean(2,3,4));
+const findSmallest = (arr) =>{
+    
+  
+    // Initialize the smallest value to the first element in the array
+    let smallest = arr[0];
+  
+    // Iterate through the array and update the smallest value if a smaller value is found
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < smallest) {
+        smallest = arr[i];
+      }
+    }
+  
+    return smallest;
+  }
