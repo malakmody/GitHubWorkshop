7d77b56407223d9a5d282094eac17ee67ebaa667
iamkokymam@gmail.com <iamkokymam@gmail.com>
2024-07-14 09:28:52 +0300

    finding the mean of numbers

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 const genRandNum = (num)=> Math.random() * num;
+const findMean = (...nums)=> {
+    const sum = nums.reduce((prev,curr)=>prev+curr)
+    return sum/nums.length;
+}
+console.log(
+    findMean(2,3,4));
